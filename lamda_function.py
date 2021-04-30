54aad18d31e1488d27fdb4a6276452d22653ded6
Derick Robinson SurfaceGO <derickdev6@gmail.com>
2021-04-29 19:06:09 -0500

    Lambda functions

---
+++ lamda_function.py
@@ -0,0 +1,11 @@
+def main():
+    # Variable id    argumento        expresion
+    #palindrome= lambda string: string == string[::-1]
+    #print(palindrome(input('Type here: ')))
+
+    #def palindrome(string): return string == string[::-1]
+    #print(palindrome(input('Type here: ')))
+
+
+if __name__ == '__main__':
+    main()
